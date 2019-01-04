d62dc0b63cf3754f55423dd4b200c691567698c1
Vladimir Radzivil <vlad@unitu.co.uk>
2019-01-04 21:58:44 +0000

    Certificates

--- SamlProject/Test.cs
+++ SamlProject/Test.cs
@@ -7,6 +7,13 @@ using ComponentSpace.SAML2.Assertions;
 using ComponentSpace.SAML2.Bindings;
 using ComponentSpace.SAML2.Profiles.SSOBrowser;
 using ComponentSpace.SAML2.Protocols;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto.Parameters;
 
 namespace SamlProject
 {
@@ -72,9 +79,7 @@ namespace SamlProject
             string relayState = null;
             if (!string.IsNullOrWhiteSpace(returnUrl))
                 relayState = RelayStateCache.Add(new RelayState(returnUrl, null));
-
-            var privateKey = @"MIIDUDCCAjgCCQDZl9Be7/58pTANBgkqhkiG9w0BAQsFADBqMQswCQYDVQQGEwJH QjERMA8GA1UECAwIU29tZXJzZXQxDTALBgNVBAcMBEJhdGgxHDAaBgNVBAoME0Jh dGggU3BhIFVuaXZlcnNpdHkxGzAZBgNVBAMMEmF1dGguYmF0aHNwYS5hYy51azAe Fw0xNzA2MTIxMjMxMjNaFw0zNzA2MDcxMjMxMjNaMGoxCzAJBgNVBAYTAkdCMREw DwYDVQQIDAhTb21lcnNldDENMAsGA1UEBwwEQmF0aDEcMBoGA1UECgwTQmF0aCBT cGEgVW5pdmVyc2l0eTEbMBkGA1UEAwwSYXV0aC5iYXRoc3BhLmFjLnVrMIIBIjAN BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw54yhGaNi9/5YADBiGWn33YcQNAI f6jagUt7uc/XTcTMoHrirLNftACRzhLXS049fwe48EC6xITe5PWNbiglEXyLmnBF KMOCN7dXDCPWKhSe9vBA2b6+2BS22jAi255WhDj65u4j1+Rg5i0r5E/YLwvG5YAw i9aKSHvvwFwy8Gwo7O3viMqlKsIKx+VV3SfC70NhvtCPh1aHbECfXr05kx5KYXnO UZRZHVLdzG9XP1+BawFcY+kFcflK9uNHUD/i36gCO0X2KciwWWHrmI5ZSR4tymUv FYAIiPklFReOqXUgj0v8pS/NqxKrRzZZWPwbTieJBS0GTt6YvCrQZVnzXQIDAQAB MA0GCSqGSIb3DQEBCwUAA4IBAQBO7NZbRXUfdaICB33BuAwOxsaXaSBkEI6tgZLx wAI/gmOEy639DxWGFuhoUeMhl9B3w5COes7VNvgy9Dl/QaZLH9p3pTBwtc92nN2J U3S4MPdGhtXXKud2DiQuGYtTnp48wbphfAGQDKhz6RI3gionZyHBOkV6Fx5XvSVj Oa7DRfawg951TKGP3OpKI4vuY3kb4hW7XEFfcCEQOcaCujckSwxU6QaI7DnDGP+O wdBmfdEj9Ey37nM0qrzI5cjTz51xJ9c0oNd3+abiGzzL7L7N+AfRVcECowo5l63j vNXAI2IiTocKRsZIWg8qn0pFts3vr7afzJMY6ZQsISwD4fLg";
-            var bytes = privateKey.GetType();
+            
             // Send the authentication request to the identity provider over the configured binding.
             switch (idpBinding)
             {
@@ -84,11 +89,19 @@ namespace SamlProject
                     break;
 
                 case Saml2SsoBinding.HttpRedirect:
+                   
+                    StreamReader sr = new StreamReader(@"C:\Users\vladi\Source\Repos\othmankh\ShibTesting\SamlProject\App_Data\encrypted_private_key.pem");
+                    PemReader pr = new PemReader(sr, new PasswordFinder("WRONG_PASSWORD"));
+                    RSAParameters rsaParams = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)pr.ReadObject());
+
+                    RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                    rsa.ImportParameters(rsaParams);
+
                     var encryptionCertificate = new X509Certificate2();
-                    //var encryptionCertificate = GetCertificate();
-                    encryptionCertificate.Import(Encoding.ASCII.GetBytes(privateKey));
-                    ServiceProvider.SendAuthnRequestByHTTPRedirect(httpContext.Response, idpLocation, authnRequestXml, relayState,
-                        encryptionCertificate.PrivateKey);
+                    encryptionCertificate.Import(@"C:\Users\vladi\Source\Repos\othmankh\ShibTesting\SamlProject\App_Data\unitu.cer");
+                    encryptionCertificate.PrivateKey = rsa;
+
+                    ServiceProvider.SendAuthnRequestByHTTPRedirect(httpContext.Response, idpLocation, authnRequestXml, relayState, encryptionCertificate.PrivateKey);
                     break;
 
                 default:
@@ -107,4 +120,21 @@ namespace SamlProject
         }
 
     }
+
+    public class PasswordFinder : IPasswordFinder
+    {
+        private string Password = String.Empty;
+
+        public PasswordFinder(string password)
+        {
+            this.Password = password;
+        }
+
+        public char[] GetPassword()
+        {
+            return Password.ToCharArray();
+        }
+    }
+    
 }
+
